cfa97031f5fbd6faf888f30b12e86ec87bb89318
anissasoungpanya <anissamsoungpanya@gmail.com>
2024-10-16 21:50:27 -0700

    changes

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,32 +6,45 @@ import React, { useState, useEffect } from "react";
 function MyApp() {
 	const [characters, setCharacters] = useState([]);
 
-function removeOneCharacter(index){
-	const updated = characters.filter((character, i) => {
-		return i !== index;
-	});
-	setCharacters(updated);
-	}
-
-function deleteUser(index, id){
-    const promise = fetch("Http://localhost:8000/users/:id", {
-    method: "DELETE",})
-
-  return promise;
-}
-
-function deleteFromList(index, id){
-        deleteUser(person)
+function removeOneCharactesr(index){
+        const deletedUser = characters[index].id;
+	fetch('http://localhost:8000/users/${deletedUser}',{
+	method: 'DELETE',
+	})
         .then((response) => {
-        if (response.status === 204){
-        const updateCharacters = characters.filter((character, i) =>
-                i !== index);
-        setCharacters(updatedCharacters);
-        } else if (response.status === 404){
-                console.log("User not found.");
-        }
-        }).catch((error) => console.log("Error:", error));
+	if (response.status === 204){
+		const updated = characters.filter(
+		(character) => character.id !== deletedUser);
+       		setCharacters(updated);
+        } else if (response.status === 404) {
+		console.log("User not found.");
+	} else {console.log("Failed to delete. Status : $ {response.status}");
+		}
+	})
+	.catch((error) => {console.log("Error:", error);
+	});
 }
+
+function removeOneCharacter(index) {
+    const deleted = characters[index].id;
+    fetch(`http://localhost:8000/users/${deleted}`, {
+      method: 'DELETE',
+    })
+    .then((response) => {
+      if (response.status === 204) {
+        const updated = characters.filter(
+          (character) => character.id !== deleted);
+        setCharacters(updated);
+      } else if (response.status === 404) {
+        console.log("User not found.");
+      } else {
+        console.log(`Failed to delete. Status code: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+  }
 
 function updateList(person) {
   postUser(person)
